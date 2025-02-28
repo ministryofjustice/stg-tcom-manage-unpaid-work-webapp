8f80e4d0ee1dd5bb5924a42a1c3138099a5184d3
geoffjustice <geoffrey.ofori@justice.gov.uk>
2025-02-28 11:58:48 +0000

    adding your progress page and your details

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -10,5 +10,25 @@ export default function routes(): Router {
     res.render('pages/index')
   })
 
+  get('/your-details', async (req, res, next) => {
+    const userProfile = {
+      name: 'Carolina Pizatto Girardi',
+      userId: 'P258053P',
+      hoursRequired: 100,
+      address: 'Flat 1\n1 Example St\nLondon\nSE1 1AA',
+      email: 'carolinapizatto@example.com',
+      phone: '07777 012345',
+    }
+    res.render('pages/details', { userProfile })
+  })
+
+  get('/your-progress', async (req, res, next) => {
+    const progress = {
+      completedHours: 40,
+      totalHours: 100,
+    }
+    res.render('pages/progress', { progress })
+  })
+
   return router
 }
