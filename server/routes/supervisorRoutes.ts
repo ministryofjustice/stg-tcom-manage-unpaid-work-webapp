fc589fea229b87ec04dd5a22ab4fa9c30a5dc6c9
geoffreykpler <gofori-ext@kpler.com>
2025-03-18 16:27:22 +0000

    feat(check-in-check-out)

--- server/routes/supervisorRoutes.ts
+++ server/routes/supervisorRoutes.ts
@@ -5,13 +5,89 @@ import placements from './data/supervisor-placements'
 export default function routes(): Router {
   const router = Router()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
+  const post = (path: string | string[], handler: RequestHandler) => router.post(path, asyncMiddleware(handler))
 
   get('/', async (req, res, next) => {
     res.render('pages/supervisor/placements', { placements })
   })
 
-  get('/view-placement', async (req, res, next) => {
-    res.render('pages/supervisor/view-placement')
+  get('/view-placement/:id', async (req, res, next) => {
+    const placementId = decodeURIComponent(req.params.id)
+    const placement = placements.find(p => p.id === placementId)
+    if (placement) {
+      const message = req.flash('success')
+      res.render('pages/supervisor/view-placement', { placement, message })
+    } else {
+      res.redirect('/supervisor')
+    }
+  })
+
+  get('/check-in/:placementId/:userId', async (req, res, next) => {
+    const placementId = decodeURIComponent(req.params.placementId)
+    const userId = decodeURIComponent(req.params.userId)
+    const placement = placements.find(p => p.id === placementId)
+    if (placement) {
+      const attendee = placement.attendees.find(a => a.userId === userId)
+      if (attendee) {
+        res.render('pages/supervisor/check-in', { placement, attendee })
+      } else {
+        res.redirect('/supervisor')
+      }
+    } else {
+      res.redirect('/supervisor')
+    }
+  })
+
+  post('/check-in/:placementId/:userId/confirm', async (req, res, next) => {
+    const placementId = decodeURIComponent(req.params.placementId)
+    const userId = decodeURIComponent(req.params.userId)
+    const placement = placements.find(p => p.id === placementId)
+    if (placement) {
+      const attendee = placement.attendees.find(a => a.userId === userId)
+      if (attendee) {
+        attendee.status = 'Checked in'
+        req.flash('success', `${attendee.name} has been checked in!`)
+        res.redirect(`/supervisor/view-placement/${placementId}`)
+      } else {
+        res.redirect('/supervisor')
+      }
+    } else {
+      res.redirect('/supervisor')
+    }
+  })
+
+  get('/checkout/:placementId/:userId', async (req, res, next) => {
+    const placementId = decodeURIComponent(req.params.placementId)
+    const userId = decodeURIComponent(req.params.userId)
+    const placement = placements.find(p => p.id === placementId)
+    if (placement) {
+      const attendee = placement.attendees.find(a => a.userId === userId)
+      if (attendee) {
+        res.render('pages/supervisor/checkout', { placement, attendee })
+      } else {
+        res.redirect('/supervisor')
+      }
+    } else {
+      res.redirect('/supervisor')
+    }
+  })
+
+  post('/checkout/:placementId/:userId/confirm', async (req, res, next) => {
+    const placementId = decodeURIComponent(req.params.placementId)
+    const userId = decodeURIComponent(req.params.userId)
+    const placement = placements.find(p => p.id === placementId)
+    if (placement) {
+      const attendee = placement.attendees.find(a => a.userId === userId)
+      if (attendee) {
+        attendee.status = 'Checked out'
+        req.flash('success', `${attendee.name} has been checked out!`)
+        res.redirect(`/supervisor/view-placement/${placementId}`)
+      } else {
+        res.redirect('/supervisor')
+      }
+    } else {
+      res.redirect('/supervisor')
+    }
   })
 
   return router
