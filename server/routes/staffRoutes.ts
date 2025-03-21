cc0f04f3a595d2b864534e28a2f9078ea341b91b
Tess Barnes <tess.barnes@justice.gov.uk>
2025-03-21 16:34:25 +0000

    WIP add photo verify page

--- server/routes/staffRoutes.ts
+++ server/routes/staffRoutes.ts
@@ -4,9 +4,18 @@ import asyncMiddleware from '../middleware/asyncMiddleware'
 export default function routes(): Router {
   const router = Router()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
+  const post = (path: string | string[], handler: RequestHandler) => router.post(path, asyncMiddleware(handler))
 
   get('/', async (req, res, next) => {
     res.render('pages/staff/cases')
+  })
+
+  get('/verify-pop-photo', async (req, res, next) => {
+    res.render('pages/staff/verify-pop-photo')
+  })
+
+  post('/verify-pop-photo', async (req, res, next) => {
+    res.render('pages/index')
   })
 
   return router
